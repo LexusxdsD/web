9857c38781fd9dc6bc6f5af790615e2c58e4b1e4
Aleksey Popov <146370497+LexusxdsD@users.noreply.github.com>
2025-02-27 23:31:34 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('buy-now').addEventListener('mouseenter', function() {
-    this.classList.add('shake'); // Добавляем класс тряски
-});
-
-document.getElementById('buy-now').addEventListener('animationend', function() {
-    this.classList.remove('shake'); // Убираем класс после завершения анимации
-});
